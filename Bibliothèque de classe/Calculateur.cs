f303170db96c268cdc33c24e9caf739497f00e64
Djefrid Byli Fotue Kuate <2210038@etu.cegepjonquiere.ca>
2023-04-23 19:28:41 -0400

    ajout des deux fonction

--- Bibliothèque de classe/Calculateur.cs
+++ Bibliothèque de classe/Calculateur.cs
@@ -39,6 +39,39 @@
             return nbre;
         }
 
+        public double VerifierSaisieDouble()
+        {
+            bool estValide = false;
+            double nbre = 0;
+
+            do
+            {
+                try
+                {
+                    string value = Console.ReadLine();
+
+                    if (!double.TryParse(value, out nbre))
+                        throw new MonException("Vous n'avez pas entrer un entier");
+                    else
+                        estValide = true;
+                }
+                catch (MonException monEx)
+                {
+                    Console.WriteLine(monEx.Message);
+                }
+                finally
+                {
+                    if (!estValide)
+                    {
+                        Console.WriteLine("Ressayer");
+                    }
+                }
+
+            } while (!estValide);
+
+            return nbre;
+        }
+
         public void Multiplier()
         {
             Console.WriteLine("Entrer un nombre a diviser ");
@@ -73,9 +106,7 @@
             try
             {
                 if (nbre2 == 0)
-                {
                     throw new DivideByZeroException();
-                }
 
                 int rslt = checked(nbre1 / nbre2);
 
@@ -90,6 +121,61 @@
             {
                 Console.WriteLine("Une erreur s'est produite ");
                 Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Exposant()
+        {
+            Console.WriteLine("Quel nombre vous vouler carluler l'exposant ? :");
+            double nbre1 = VerifierSaisieDouble();
+            Console.WriteLine("A quel nombre d'exopsant vouler vous calculer le nombre precedant ? :");
+            double nbre2 = VerifierSaisieDouble();
+
+            try
+            {
+                double rslt = checked(Math.Pow(nbre1, nbre2));
+
+                Console.WriteLine($"Le resultat de l'operation est : {rslt}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("L'argument est hors plage : " + ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("L'argument est invalide : " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Une erreur s'est produite : " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// fonction qui calcule le logarithme d'un nombre 
+        /// </summary>
+        public void Logarithme()
+        {
+            Console.WriteLine("Quel nombre vous vouler carluler le logarithme ? :");
+            double nbre = VerifierSaisieDouble();
+
+            try
+            {
+                double rslt = checked(Math.Log(nbre));
+
+                Console.WriteLine($"Le resultat de l'operation est : {rslt}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("L'argument est hors plage : " + ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("L'argument est invalide : " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Une erreur s'est produite : " + ex.Message);
             }
         }
     }
